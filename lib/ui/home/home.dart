0deca51674311115fc7a9815b2f59c5884256639
mehdi <mehdiprogrammer30@gmail.com>
2022-09-19 23:16:43 +0430

    Piyadehsazi Slider page Asli

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,8 +1,12 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nike_ecommerce_flutter/data/banner.dart';
 import 'package:nike_ecommerce_flutter/data/repo/banner_repository.dart';
 import 'package:nike_ecommerce_flutter/data/repo/product_repository.dart';
 import 'package:nike_ecommerce_flutter/ui/home/bloc/home_bloc.dart';
+import 'package:nike_ecommerce_flutter/ui/widgets/image.dart';
+import 'package:nike_ecommerce_flutter/ui/widgets/slider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -24,15 +28,22 @@ class HomeScreen extends StatelessWidget {
             builder: (context, state) {
               if (state is HomeSuccess) {
                 return ListView.builder(
-                  padding: const EdgeInsets.fromLTRB(12, 12, 12, 100),
+                  // padding: const EdgeInsets.fromLTRB(12, 12, 12, 100),
                   itemCount: 5,
                   itemBuilder: (context, index) {
                     switch (index) {
                       case 0:
-                        return Image.asset(
-                          'assets/img/nike_logo.png',
-                          height: 39,
+                        return Container(
+                          height: 56,
+                          alignment: Alignment.center,
+                          child: Image.asset(
+                            'assets/img/nike_logo.png',
+                            height: 24,
+                            fit: BoxFit.fitHeight,
+                          ),
                         );
+                      case 2:
+                        return BannerSlider(banners: state.banners,);
                       default:
                         return Container();
                     }
@@ -64,3 +75,5 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+
---
+++ lib/ui/widgets/slider.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:nike_ecommerce_flutter/data/banner.dart';
+import 'package:nike_ecommerce_flutter/ui/widgets/image.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class BannerSlider extends StatelessWidget {
+  final PageController _controller = PageController();
+  final List<BannerEntity> banners;
+  BannerSlider({
+    Key? key,
+    required this.banners,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 2,
+      child: Stack(
+        children: [
+          PageView.builder(
+            physics: BouncingScrollPhysics(),
+            controller: _controller,
+            itemCount: banners.length,
+            itemBuilder: (context, index) => Padding(
+              padding: const EdgeInsets.only(left: 12, right: 12),
+              child: ImageLoadingService(
+                imageUrl: banners[index].imageUrl,
+                borderRadius: BorderRadius.circular(12),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 8,
+            child: Center(
+              child: SmoothPageIndicator(
+                controller: _controller,
+                count: banners.length,
+                axisDirection: Axis.horizontal,
+                effect: WormEffect(
+                  spacing: 4.0,
+                  radius: 4.0,
+                  dotWidth: 20.0,
+                  dotHeight: 3.0,
+                  paintStyle: PaintingStyle.fill,
+                  dotColor: Colors.grey.shade400,
+                  activeDotColor: Theme.of(context).colorScheme.onBackground,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
